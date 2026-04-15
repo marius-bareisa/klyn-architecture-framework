d8ce532cc4004c0b5e9b3550356f016cee4329aa
Marius Bareiša <m.bareisa@gmail.com>
2026-04-15 09:13:01 +0300

    catch-IOException-correctly

--- tmdb-impl/src/main/java/com/klynaf/tmdb/impl/util/SafeFlow.kt
+++ tmdb-impl/src/main/java/com/klynaf/tmdb/impl/util/SafeFlow.kt
@@ -1,5 +1,6 @@
 package com.klynaf.tmdb.impl.util
 
+import com.klynaf.core.domain.error.NetworkException
 import com.klynaf.core.domain.util.Result
 import com.klynaf.tmdb.impl.mapper.toResult
 import kotlinx.coroutines.flow.Flow
@@ -7,9 +8,7 @@ import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import retrofit2.Response
-
-fun <T> safeFlow(block: suspend FlowCollector<Result<T>>.() -> Unit): Flow<Result<T>> =
-    flow(block).catch { e -> emit(Result.Error(e)) }
+import java.io.IOException
 
 fun <T, R> fetchFlow(
     call: suspend () -> Response<T>,
@@ -22,3 +21,11 @@ fun <T, R> fetchFlow(
         is Result.Loading -> Unit
     }
 }
+
+private fun <T> safeFlow(block: suspend FlowCollector<Result<T>>.() -> Unit): Flow<Result<T>> =
+    flow(block).catch { e ->
+        when (e) {
+            is IOException -> emit(Result.Error(NetworkException(e)))
+            else -> emit(Result.Error(e))
+        }
+    }
